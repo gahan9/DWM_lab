b188a67103df5ef5ec797f3e36717081edaa82c7
gahan9 <gahansaraiya@gmail.com>
2019-03-15 15:36:51 +0530

    added example of aggregation

--- practical3/src/aggregate.py
+++ practical3/src/aggregate.py
@@ -1,5 +1,25 @@
-from __future__ import print_function
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+"""
+Author: Gahan Saraiya
+GiT: https://github.com/gahan9
+StackOverflow: https://stackoverflow.com/users/story/7664524
+
+Aggregation
+"""
+
 from cubes import Workspace, Cell, PointCut
+
+
+def pretty_print(browser, dimension, **kwargs):
+    cell = kwargs.get('cell', None)
+    if cell:
+        for row in browser.aggregate(cell, drilldown=[dimension]).table_rows(dimension):
+            print("{:>8s} >>> {}".format(row.label, row.record))
+    else:
+        for row in browser.aggregate(drilldown=[dimension]).table_rows(dimension):
+            print("{:>8s} >>> {}".format(row.label, row.record))
+
 
 # 1. Create a workspace
 workspace = Workspace()
@@ -7,17 +27,15 @@ workspace.register_default_store("sql", url="sqlite:///data.sqlite")
 workspace.import_model("model.json")
 
 # 2. Get a browser
-browser = workspace.browser("irbd_balance")
+browser = workspace.browser("black_friday")
 
 # 3. Play with aggregates
 result = browser.aggregate()
 
-print("Total\n"
-      "----------------------")
+print("Total\n"+"-"*25)
 
 print("Record count : %8d" % result.summary["record_count"])
 print("Total Purchase : %8d" % result.summary["Purchase_sum"])
-print("Double Purchase: %8d" % result.summary["double_Purchase_sum"])
 
 #
 # 4. Drill-down through a dimension
@@ -32,26 +50,26 @@ result = browser.aggregate(drilldown=["Age"])
 print(("%-20s%10s%10s%10s\n"+"-"*50) % ("Category", "Count", "Total", "Double"))
 #
 for row in result.table_rows("Age"):
-    print("%-20s%10d%10d%10d" % ( row.label,
+    print("%-20s%10d%10d" % ( row.label,
                               row.record["record_count"],
-                              row.record["Purchase_sum"],
-                              row.record["double_Purchase_sum"])
-                              )
-
+                              row.record["Purchase_sum"]
+                              ))
+"""
 print("\n"
       "Slice where Category = Age group 0-17\n"
       "==================================================")
 
 cut = PointCut("Age", ["0-17"])
-cell = Cell(browser.cube, cuts = [cut])
+cell = Cell(browser.cube, cuts=[cut])
 
 result = browser.aggregate(cell, drilldown=["Age"])
 
 print(("%-20s%10s%10s%10s\n"+"-"*50) % ("Sub-category", "Count", "Total", "Double"))
 
-for row in result.table_rows("Age"):
-    print("%-20s%10d%10d%10d" % ( row.label,
-                              row.record["record_count"],
-                              row.record["Purchase_sum"],
-                              row.record["double_Purchase_sum"],
-                              ))
+pretty_print(browser, dimension="Age", cell=cell)
+# for row in result.table_rows("Age"):
+#     print("%-20s%10d%10d%10d" % ( row.label,
+#                               row.record["record_count"],
+#                               row.record["Purchase_sum"]
+#                               ))
+"""
