bf693cf3528b991ce2fc31150e7386c6edf45607
gahan9 <gahansaraiya@gmail.com>
2019-04-17 12:07:38 +0530

    optimize code running time, added progress bar and modified precision to 5

--- term_paper/main.py
+++ term_paper/main.py
@@ -1,18 +1,22 @@
+import os
 import simulator
 import itertools
+from time import sleep
 from datetime import datetime
+
+PRECISION = 5
 
 def generate_file_1():
     with open('file1.txt', 'w') as fp:
-        for df in simulator.get_sequential_data():
+        for df in simulator.get_sequential_data(precision=PRECISION):
             fp.write("{}, {}, {}, {}\n".format(
-                df['client'], df['latitude'], df['longitude'], df['timestamp']))
+                df['taxi_id'], df['latitude'], df['longitude'], df['timestamp']))
 
 def generate_file_2():
     with open('file2.txt', 'w') as fp:
-        for df in simulator.get_sequential_data():
+        for df in simulator.get_sequential_data(precision=PRECISION):
             fp.write("{}, {}, {}\n".format(
-                df['client'], df['geohash'], df['timestamp']))
+                df['taxi_id'], df['geohash'], df['timestamp']))
 
 
 def parseline(line):
@@ -22,21 +26,29 @@ def parseline(line):
     epoch_time = datetime.strptime(timestamp, ' %Y-%m-%d %H:%M:%S').timestamp()
     return geohash, epoch_time, taxi_id
 
-def generate_file_3():
+def generate_file_3(prevent_skip=False):
+    records_to_process = int(os.popen("wc -l file2.txt").read().strip().split()[0])
+    print("Processing Dissimilarity computation for {} records..".format(records_to_process))
+    # Initial call to print 0% progress
+    simulator.printProgressBar(0, records_to_process-1, prefix = 'Progress:', suffix = 'Complete', length = 50)
     with open('file3.txt', 'w') as write_file:
         skip_n = 0
         with open('file2.txt', 'r') as file1:
+            count = 0
             for file1_line in file1.readlines():
                 skip_n += 1
+                count += 1
                 dataset1 = parseline(file1_line)
+                simulator.printProgressBar(count, records_to_process, prefix = 'Progress:', suffix = 'Complete', length = 50)
                 with open('file2.txt', 'r') as file2:
                     line_no = 0
                     for file2_line in file2.readlines():
                         line_no += 1
-                        if line_no > skip_n:
+                        if line_no > skip_n or prevent_skip:
                             dataset2 = parseline(file2_line)
-                            write_file.write("{}, {}, {}\n".format(
-                                dataset1[0], dataset2[0], dataset1[1] - dataset2[1]))
+                            if dataset1[2] == dataset1[2] and dataset1 != dataset2 and dataset1[0] != dataset2[0]:
+                                cmp_str = "{}-{}".format(dataset1[2], dataset2[2])
+                                write_file.write("{}, {}, {}, {} \n".format(cmp_str, dataset1[0], dataset2[0], dataset1[1] - dataset2[1]))
 
 
 if __name__ == '__main__':
--- term_paper/simulator.py
+++ term_paper/simulator.py
@@ -5,14 +5,14 @@ datasets_dir = os.path.abspath(os.path.join(os.path.dirname(__file__), 'datasets
 dataset_files = [os.path.join(datasets_dir, file) for file in os.listdir(datasets_dir)]
 
 
-def parseline(line):
+def parseline(line, precision=5):
     if line[-1] == '\n':
         line = line[:-1]
-    client_name, timestamp, latitude, longitude = line.split(',')
+    taxi_id, timestamp, latitude, longitude = line.split(',')
     latitude, longitude = float(latitude), float(longitude)
-    gh = geohash.encode(latitude, longitude, precision=3)
+    gh = geohash.encode(latitude, longitude, precision=precision)
     return {
-        'client': client_name,
+        'taxi_id': taxi_id,
         'geohash': gh,
         'latitude': latitude,
         'longitude': longitude,
@@ -20,17 +20,17 @@ def parseline(line):
     }
 
 
-def nextline(dataset_file):
-    client_name = dataset_file.split('.')[0]
-    client_name = client_name.split('/')[-1]
+def nextline(dataset_file, precision):
+    taxi_id = dataset_file.split('.')[0]
+    taxi_id = taxi_id.split('/')[-1]
     with open(dataset_file, mode='r') as fp:
         for line in fp.readlines():
-            yield parseline(line)
+            yield parseline(line, precision)
     yield None
 
 
-def get_random_data():
-    file_pointers = [nextline(dataset_file) for dataset_file in dataset_files]
+def get_random_data(precision):
+    file_pointers = [nextline(dataset_file, precision) for dataset_file in dataset_files]
     while len(file_pointers) > 0:
         for fp in file_pointers:
             line = fp.__next__()
@@ -41,10 +41,31 @@ def get_random_data():
                 yield line
 
 
-def get_sequential_data():
-    file_pointers = [nextline(dataset_file) for dataset_file in dataset_files]
+def get_sequential_data(precision):
+    file_pointers = [nextline(dataset_file, precision) for dataset_file in dataset_files]
     for fp in file_pointers:
         line = fp.__next__()
         while line is not None:
             yield line
             line = fp.__next__()
+
+# Print iterations progress
+def printProgressBar (iteration, total, prefix = '', suffix = '', decimals = 1, length = 100, fill = '█'):
+    """
+    Call in a loop to create terminal progress bar
+    @params:
+        iteration   - Required  : current iteration (Int)
+        total       - Required  : total iterations (Int)
+        prefix      - Optional  : prefix string (Str)
+        suffix      - Optional  : suffix string (Str)
+        decimals    - Optional  : positive number of decimals in percent complete (Int)
+        length      - Optional  : character length of bar (Int)
+        fill        - Optional  : bar fill character (Str)
+    """
+    percent = ("{0:." + str(decimals) + "f}").format(100 * (iteration / float(total)))
+    filledLength = int(length * iteration // total)
+    bar = fill * filledLength + '-' * (length - filledLength)
+    print('\r%s |%s| %s%% %s' % (prefix, bar, percent, suffix), end = '\r')
+    # Print New Line on Complete
+    if iteration == total: 
+        print()
